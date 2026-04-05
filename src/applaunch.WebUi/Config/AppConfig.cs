d944d45009e2589ad2c2269e22a99d525b0bace8
duivos <72436899+duivos@users.noreply.github.com>
2026-04-05 11:23:44 +0200

    Add config and refactor app scanning

---
+++ src/applaunch.WebUi/Config/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace applaunch.WebUi.Config;
+
+internal static class AppConfig
+{
+    public static readonly string[] ProgramPaths =
+    [
+        Environment.GetFolderPath(Environment.SpecialFolder.CommonPrograms),
+        Environment.GetFolderPath(Environment.SpecialFolder.Programs),
+    ];
+
+    public static string[] ExcludeKeywords = ["uninstall", "readme", "help"];
+}
--- src/applaunch.WebUi/Models/AppItems.cs
+++ src/applaunch.WebUi/Models/AppItems.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace applaunch.WebUi.Models;
 
-namespace applaunch.WebUi.Models
+public class AppItem
 {
-    public class AppItem
-    {
-        public required string Name { get; set; }
-        public required string Path { get; set; }
-    }
+    public required string Name { get; set; }
+    public required string Path { get; set; }
 }
