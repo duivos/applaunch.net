897e141b9329feda7bc65fd416fa260e35589084
duivos <72436899+duivos@users.noreply.github.com>
2026-04-05 10:59:36 +0200

    make filepath required

--- src/applaunch.WebUi/Models/AppItems.cs
+++ src/applaunch.WebUi/Models/AppItems.cs
@@ -7,6 +7,6 @@ namespace applaunch.WebUi.Models
     public class AppItem
     {
         public required string Name { get; set; }
-        public string Path { get; set; }
+        public required string Path { get; set; }
     }
 }
